3ceb993a06e72cbcb02f9b6b19f7dd3921dd52c9
daniel4o <danielddg.ss@gmail.com>
2022-07-03 19:08:55 +0300

    Started front-end. Added components folder with todo,  modal and backdrop. Added custom css for the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import './App.css';
+import Backdrop from './components/Backdrop';
+import Todo from './components/Todo';
+import Modal from './components/Modal';
 
 function App() {
-  return 
+  return (
+  <div>
+    <h1>My Todos</h1>
+    <Todo text='My journey'/>
+  </div> 
+  )
 }
 
 export default App;
